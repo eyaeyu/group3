282c2fa80214a5bb9ff0314ea6082007ddd81ccf
khadar072 <khadarmoha53@gmail.com>
2023-06-28 15:24:49 +0300

    this change make by khadar

--- main.java
+++ main.java
@@ -2,9 +2,20 @@ public class main{
     void eyob(){
         System.out.println("i am eyob chiksa");
     }
+public class add{
+    int x=10;
+    int y5;
+    int z=x*y;
+    void show(){
+        System.out.println("i am khadar ");
+        System.out.println("z = " z);
+        
+    }
 
 public static  void main(String[] args) {
    main s=new main();
    s.eyob(); 
+   add k=new add();
+   k.show(); 
 }
 }
