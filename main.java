827e94ff6d8ec04ebdb4e1b90376e26993df2be8
hassen <hmyws187404@gmail.com>
2023-06-30 21:09:57 +0300

    this is a commit from hassen

--- main.java
+++ main.java
@@ -1,4 +1,27 @@
-public class main{
+
+//this was from khadar
+ class add{
+    int x=10;
+    int y=5;
+    int z=x+y;
+    void show(){
+        System.out.println("i am khadar ");
+        System.out.println("z = " + z);
+        
+    }
+}
+class Smile{
+        void Showsmile(){
+            String id="DBUR/0280/13";
+            System.out.println("-------------------------------------------------------------");
+             System.out.println();
+            System.out.println("Hello this is Hassen with id :"+ id);  
+            System.out.println();
+            System.out.println("-------------------------------------------------------------");
+        }
+    }
+
+public class Main{
     void eyob(){
         System.out.println("i am eyob chiksa");
         System.out.println("i am a software engineer");
@@ -12,21 +35,15 @@ public class main{
         System.out.println("id 0395/13");
         System.out.println("software engineer");
     }
-public class add{
-    int x=10;
-    int y5;
-    int z=x*y;
-    void show(){
-        System.out.println("i am khadar ");
-        System.out.println("z = " z);
-        
-    }
+    
 
 public static  void main(String[] args) {
-   main s=new main();
+   Main s=new Main();
    s.eyob(); 
    s.hareg();
    add k=new add();
    k.show(); 
+Smile S = new Smile();
+S.Showsmile();
 }
 }
